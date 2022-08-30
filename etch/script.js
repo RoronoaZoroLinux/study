5f966c2bfebf63f4dd02c12ed3d43be5f86a3a2e
zoro windows dektop <roronoazorolinux@gmail.com>
2022-08-31 00:44:51 +0300

    last commit before changing to grid template

--- etch/script.js
+++ etch/script.js
@@ -1,21 +1,103 @@
+//newDiv.addEventListener('click' , e=> {e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`}) 
 const continer = document.querySelector('.container');
+document.getElementById('btn_rainbow').addEventListener('click' , e=>{ selectedColor = 'rainbow' ; eraser = false;});
+document.getElementById('btn_black').addEventListener('click' , e=>{ selectedColor = 'black' ; eraser = false;});
+document.getElementById('btn_eraser').addEventListener('click' , e=>{ selectedColor = 'white'; eraser = true; });
+document.getElementById('btn_grid').addEventListener('click', toggleGrid);
+document.getElementById('btn_clear').addEventListener('click', clear);
 
 
+let eraser = false;
+let gridEnabled = true;
+let selectedColor = "black";
 
-function build(){
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
-    let newDiv = document.createElement("div");
 
-    newDiv.classList.add('newDiv');
-    newDiv.addEventListener("mouseover" , e => { e.target.style.backgroundColor = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`});
+function changeColor(e){
     
-    continer.appendChild(newDiv); 
+    if(e.type == "mouseover" && !mouseDown) return;
+
+    if(selectedColor == 'rainbow'){
+        
+        let rgb = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+       
+        if(e.target.dataset.serial % 65 != 0);{
+
+            document.querySelector(`[data-serial = '${e.target.dataset.serial}']`).style.backgroundColor = 'black';
+            document.querySelector(`[data-serial = '${e.target.dataset.serial -1}']`).style.backgroundColor = 'green';
+            document.querySelector(`[data-serial = '${Number(e.target.dataset.serial) + Number(1) }']`).style.backgroundColor = 'red';
+
+        }
+
+   
+        
+    }
+    else{
+        if(!eraser){
+        document.querySelector(`[data-serial = '${e.target.dataset.serial}']`).style.backgroundColor = 'black';
+        document.querySelector(`[data-serial = '${e.target.dataset.serial -1}']`).style.backgroundColor = 'blue';
+        }
+        else{
+            e.target.style.backgroundColor = 'white';
+        }
+    }
+
+
 
 }
 
-for(let i = 0 ; i < 2210 ; i++ ){
+function clear(){
+
+    Array.from(document.querySelectorAll('.newDiv')).forEach( element => {
+        element.style.backgroundColor= "white";
+      } );
+
+}
+
+function build(i){
+
+        let newDiv = document.createElement("div");
+        newDiv.classList.add('newDiv');
+        newDiv.dataset.serial = i;
+        continer.appendChild(newDiv); 
+        newDiv.addEventListener('click' , changeColor);
+        newDiv.addEventListener('mouseover' , changeColor);
+
+
+}
+
+function toggleGrid(){
+
+       if(gridEnabled){
+
+        Array.from(document.querySelectorAll('.newDiv')).forEach( element => {
+            element.style.border = "hidden";
+          } );
+
+          gridEnabled = false;
+
+       }
+
+       else{
+
+
+        Array.from(document.querySelectorAll('.newDiv')).forEach( element => {
+            element.style.border = "1px solid dimgray";
+          } );
+
+          gridEnabled = true;
+
+       }
+
+
+}
+
+for(let i = 0 ; i < 2220 ; i++ ){
     
-   build(); 
+   build(i); 
 
 }
 
