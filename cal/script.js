d69cba48916fdea35a3e50bdba70ac10bfae1f8c
zoro windows dektop <roronoazorolinux@gmail.com>
2022-09-09 02:52:56 +0300

    some boilerplate

--- cal/script.js
+++ cal/script.js
@@ -2,26 +2,96 @@ const screen = document.querySelector(".column2");
 const small = document.querySelector(".column1");
 const numbers = Array.from(document.querySelectorAll(".num")); 
 const operators = Array.from(document.querySelectorAll(".op")); 
+let memslot1 = "";
+let memslot2 = "";
+let memslot3 = null;
+let op_second = false;
+let operation = null;
+
+
 
 numbers.forEach( key =>{
 
     key.addEventListener("click" , e=>{
-
-        screen.innerText = e.target.dataset.key;
+    
+        
+        if(op_second === true){
+            if(memslot2.length < 10){
+                
+                memslot2 += e.target.dataset.key;
+                screen.innerText = memslot2;
+            }
+            return;
+        }
+        
+        if(memslot1.length < 10){
+            
+            small.innerText ="";
+            memslot1 += e.target.dataset.key;
+            screen.innerText = memslot1;
+        }
 
     })
 
 
 } )
 
-operators.forEach(key => {
+const f_del = function(e){
+    
 
-    key.addEventListener("click", e=> {
-        small.innerText = e.target.dataset.key;
-    })
+if(op_second === true){
+    let foo = Array.from(memslot2);
+    foo.pop();
+    memslot2 = foo.join('');
+    
+    if(memslot2.length == 0){
+        screen.innerText = 0;
+        return;
+    }
+    screen.innerText = memslot2;
+    return;
 
-})
+}
 
-console.log(operators)
-console.log(numbers)
+let foo = Array.from(memslot1);
+foo.pop();
+memslot1 = foo.join('');
 
+if(memslot1.length == 0){
+    screen.innerText = 0;
+    return;
+}
+screen.innerText = memslot1;
+
+}
+
+const f_plus = function(e){
+
+op_second = true;
+operation = "PLUS";
+small.innerText = memslot1;
+
+}
+
+const f_result = function(){
+
+switch(operation){
+
+    case "PLUS":
+        memslot3 = parseFloat(memslot1) + parseFloat(memslot2);
+        screen.innerText = memslot3;
+        break;
+
+}
+
+small.innerText = memslot1 + " + " + memslot2;
+
+op_second = false;
+memslot2 = "";
+memslot1 = String(memslot3);
+
+
+}
+document.querySelector("[data-key='PLUS']").addEventListener("click" , f_plus);
+document.querySelector("[data-key='DEL']").addEventListener("click" , f_del);
+document.querySelector("[data-key='RESULT']").addEventListener("click" , f_result);
